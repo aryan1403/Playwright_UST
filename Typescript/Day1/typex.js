a3ae8e9e4d307e90ab9095c9ac8f0cdb5fb773b5
aryan1403 <aaryan14032006@gmail.com>
2025-04-18 10:11:05 +0530

    custom types done

--- Typescript/Day1/typex.js
+++ Typescript/Day1/typex.js
@@ -1,13 +1,14 @@
 var UserAccount = /** @class */ (function () {
-    function UserAccount(name, id) {
+    function UserAccount(name, id, role) {
         this.name = name; // similar java
         this.id = id;
+        this.role = role;
     }
     return UserAccount;
 }());
 function main() {
-    var user = new UserAccount("Aaryan", 1); // User user = new UserAccount("Aaryan", 1);
-    var user2 = new UserAccount("Kirti", 2);
-    console.log(user.name, user2.name);
+    var user = new UserAccount("Aaryan", 1, "Trainer"); // User user = new UserAccount("Aaryan", 1);
+    var user2 = new UserAccount("Kirti", 2, "Tester");
+    console.log(user.role, user2.role);
 }
 main();
--- Typescript/Day1/typex.ts
+++ Typescript/Day1/typex.ts
@@ -1,23 +1,28 @@
+type roles = "Trainer" | "Developer" | "Tester";
+
 interface User {
   name: string; //String name;
   id: number; // int id;
+  role: roles;
 }
 
 class UserAccount {
   name: string; // String name
   id: number; // int id
+  role: roles;
 
-  constructor(name: string, id: number) { // UserAccount(String name, int id) {}
+  constructor(name: string, id: number, role: roles) { // UserAccount(String name, int id) {}
     this.name = name; // similar java
     this.id = id;
+    this.role = role;
   }
 }
 
 function main() {
-    const user: User = new UserAccount("Aaryan", 1); // User user = new UserAccount("Aaryan", 1);
-    const user2: User = new UserAccount("Kirti", 2);
+    const user: User = new UserAccount("Aaryan", 1, "Trainer"); // User user = new UserAccount("Aaryan", 1);
+    const user2: User = new UserAccount("Kirti", 2, "Tester");
     
-    console.log(user.name, user2.name);
+    console.log(user.role, user2.role);
 }
 
 main();
