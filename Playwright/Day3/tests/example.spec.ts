9291c14b6c9247afe2b0cc0f83f55cdd19fb4401
aryan1403 <aaryan14032006@gmail.com>
2025-04-25 15:42:30 +0530

    parallelism

--- Playwright/Day3/tests/example.spec.ts
+++ Playwright/Day3/tests/example.spec.ts
@@ -1,17 +1,30 @@
 import { test, expect } from '@playwright/test';
-import { TodoPage } from './todoPage';
-
+/*
 test.beforeEach(async ({ TodoPage }) => {
   //await TodoPage.
-})
+})*/
+
+test.describe.configure({ mode: 'parallel' }) // serial
 
 // built in fixtures --> page, context, browser, browserName, request
-test('has title', async ({ page, browserName, TodoPage }) => {
+test('has title', async ({ page, browserName }) => {
   await page.goto('https://playwright.dev/');
 
-  if(browserName === 'chromium') {
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
-  }
+test('has title1', async ({ page, browserName }) => {
+  await page.goto('https://playwright.dev/');
+
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('has title2', async ({ page, browserName }) => {
+  await page.goto('https://playwright.dev/');
+
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
